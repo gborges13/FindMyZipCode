7f69223f0916dad920e44f8436b943d545c6c1b9
guilherme.freitas <guilherme.freitas@luizalabs.com>
2021-02-20 13:06:07 -0300

    adding swagger documentation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./../swagger.json');
 const routes = require('./api/zipCode/zipCode-routes')
 
 const app = express()
@@ -7,6 +9,7 @@ const port = process.env.PORT || 3000
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
 app.use(routes)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
