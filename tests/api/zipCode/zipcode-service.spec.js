32f571ee0f0dac9689ca2b4118acdc749f16de3c
guilherme.freitas <guilherme.freitas@luizalabs.com>
2021-02-22 19:17:35 -0300

    create tests on zipCode-service

--- tests/api/zipCode/zipcode-service.spec.js
+++ tests/api/zipCode/zipcode-service.spec.js
@@ -1,6 +1,46 @@
-describe('Test branches resource', () => {
+const zipCodeService = require('../../../src/api/zipCode/zipcode-service')
+const zipCodeRepositoryHelper = require('../../../src/api/zipCode/zipcode-repository')
 
-    test('Teste', () => {
-        expect(1 + 2).toBe(3)
+const zipCodeRecord = {
+    zipCode : '14400000',
+    address : 'Av. Alonso y Alonso',
+    district: 'Centro',
+    city: 'Franca',
+    state: 'SP'
+}
+
+
+describe('Tests on zipCode subject at service responsability', () => {
+
+    test('Test findZipCode method when zip code is located on first search', async () => {
+        const mockRepository = jest.spyOn(zipCodeRepositoryHelper, 'findOne')
+        mockRepository.mockReturnValue(zipCodeRecord)
+
+        const result = zipCodeService.findZipCode('14400000')
+        expect(result).toBe(zipCodeRecord)
+
     })
+
+    test('Test findZipCode method when zip code not is located on first search and can`t find any record', async () => {
+        const mockRepository = jest.spyOn(zipCodeRepositoryHelper, 'findOne')
+        mockRepository.mockReturnValue(null)
+
+        const result = zipCodeService.findZipCode('19999999')
+        expect(result).toBe(null)
+    })
+
+    test('Test findZipCode method when zip code not is located on first search and find the last record', async () => {
+        jest.restoreAllMocks()        
+        const mockedResult = {
+            zipCode : "10000000",
+            address : "Avenida Paulista",
+            district: "Centro",
+            city: "São Paulo",
+            state: "SP"
+        }
+
+        const result = zipCodeService.findZipCode('17777777')
+        expect(result).toEqual(mockedResult)
+    })
+
 })
